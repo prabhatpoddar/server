16f2d48e0baee5ca48350e0a67a7045697c696c7
prabhat poddar <pk041222@gmail.com>
2023-02-01 14:45:17 +0530

    new

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 
-const connect = require("./src/config/db");
-const authRoutes = require("./src/routes/auth.routes");
-const usersRoutes = require("./src/routes/users.routes");
-const wishlistRouter = require("./src/routes/wishlist.routes");
-const bagRouter = require("./src/routes/bag.routes");
-const orderRouter = require("./src/routes/order.routes");
-const productRouter = require("./src/routes/product.routes");
 const app = express();
 app.use(express.json());
 app.use(
@@ -18,19 +10,7 @@ app.use(
 );
 
 app.get("/", (req, res) => [res.send("Home Page")]);
-app.use("/auth", authRoutes);
-app.use("/users", usersRoutes);
-app.use("/product", productRouter);
-app.use("/wishlist", wishlistRouter);
-app.use("/bag", bagRouter);
-app.use("/order", orderRouter);
-app.listen(process.env.PORT, () => {
-  try {
-    connect.then((res) => {
-      console.log("db is connected");
-    });
-    console.log(`Server is running at port ${process.env.PORT}`);
-  } catch (error) {
-    console.log("error:", error);
-  }
+
+app.listen(8080, () => {
+  console.log(`Server is running at port 8080`);
 });
--- src/config/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose =require("mongoose");
-require("dotenv").config()
-mongoose.set('strictQuery', true);
-
-const connect=mongoose.connect(`${process.env.MONGO_URL}`)
-
-module.exports=connect
